748ee9b6eb1b4feb29ce45644c74d7c568122aba
sepide eslamnezhad <sepide.eslamnezhad@gmail.com>
2020-04-03 19:21:51 +0430

    started login component and authentication

--- src/app/component/Main.jsx
+++ src/app/component/Main.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "./Dashboard";
+import { ConnectedLogin } from "./Login";
 import { Router, Route } from "react-router-dom";
 import { history } from "../store/history";
 import { ConnectedNavigation } from "./Navigation";
 import { ConnectTaskDetail } from "./TaskDetail";
+import { Redirect } from "react-router";
+
+const RouteGuard = Component => ({ match }) => {
+  console.info("Route guard", match);
+  if (!store.getState().session.authenticated) {
+    return <Redirect to="/" />;
+  }
+  {
+    return <Component match={match} />;
+  }
+};
 
 const Main = () => {
   return (
     <Router history={history}>
       <Provider store={store}>
         <ConnectedNavigation />
-        <Route exact path="/dashboard" render={() => <ConnectedDashboard />} />
+        <Route exact path="/" component={ConnectedLogin} />
         <Route
           exact
-          path="/task/:id"
-          render={({ match }) => <ConnectTaskDetail match={match} />}
+          path="/dashboard"
+          render={RouteGuard(ConnectedDashboard)}
         />
+        <Route exact path="/task/:id" render={RouteGuard(ConnectTaskDetail)} />
       </Provider>
     </Router>
   );
